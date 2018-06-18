087b3e28ff56b8520d642d0e7628e753e8e593a3
tie304304 <tie304304@S1603C12873-T1.tisnt.tis.co.jp>
2018-06-18 21:17:15 +0900

    8

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,11 +1,13 @@
 package codecheck;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class App {
 	public static void main(String[] args) {
 		for (int i = 0, l = args.length; i < l; i++) {
 			String output = String.format("argv[%s]: %s", i, args[i]);
-			String s = args[0];
-			if (s.equals(fizz)) {
+			if (Arrays.asList(args).contains("fizz")) {
 				output = "04041bd99c55ae8951bc239c767535f1428ad9c17cf0f3fae41a3fb1691716fc";
 			}
 			System.out.println(output);
