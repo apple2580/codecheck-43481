e00748fd7a85db4fb4ad455f7223a2b08a67e63a
tie304304 <tie304304@S1603C12873-T1.tisnt.tis.co.jp>
2018-06-18 21:08:00 +0900

    5

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -4,7 +4,7 @@ public class App {
 	public static void main(String[] args) {
 		for (int i = 0, l = args.length; i < l; i++) {
 			String output = String.format("argv[%s]: %s", i, args[i]);
-			if (args[i] == "fizz") {
+			if (args[0].equals(fizz)) {
 				output = "04041bd99c55ae8951bc239c767535f1428ad9c17cf0f3fae41a3fb1691716fc";
 			}
 			System.out.println(output);
