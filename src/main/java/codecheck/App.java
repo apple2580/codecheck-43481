bed950390ffcf41f4f326e6767aadf552a87ea5d
tie304304 <tie304304@S1603C12873-T1.tisnt.tis.co.jp>
2018-06-18 20:57:29 +0900

    1

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -4,6 +4,9 @@ public class App {
 	public static void main(String[] args) {
 		for (int i = 0, l = args.length; i < l; i++) {
 			String output = String.format("argv[%s]: %s", i, args[i]);
+			if (args[i] = "fizz") {
+				output = "hoge"
+			}
 			System.out.println(output);
 		}
 	}
